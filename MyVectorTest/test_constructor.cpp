d35c8d7064b638b11664bc22495ca4c06e2649a5
minus9d <minus9d@gmail.com>
2015-10-27 23:31:36 +0900

    add tests for special member functions

--- MyVectorTest/test_constructor.cpp
+++ MyVectorTest/test_constructor.cpp
@@ -6,18 +6,23 @@
 TEST_CASE("constructor")
 {
     mystd::myvector<int> mv(10, 777);
-    std::cout << "index, val = (" << 5 << ", " << mv[5] << ")" << std::endl;
     REQUIRE(mv.size() == 10);
     REQUIRE(mv[0] == 777);
     REQUIRE(mv[9] == 777);
 
     auto mv2(mv);
-    std::cout << "index, val = (" << 5 << ", " << mv2[5] << ")" << std::endl;
+    REQUIRE(mv.size() == mv2.size());
+    REQUIRE(mv[0] == mv2[0]);
+    REQUIRE(mv[9] == mv2[9]);
 
     mystd::myvector<int> mv3;
     mv3 = mv;
-    std::cout << "index, val = (" << 5 << ", " << mv3[5] << ")" << std::endl;
+    REQUIRE(mv.size() == mv3.size());
+    REQUIRE(mv[0] == mv3[0]);
+    REQUIRE(mv[9] == mv3[9]);
 
     mystd::myvector<int> mv4 = std::move(mv3);
-    //std::cout << "index, val = (" << 5 << ", " << mv3[5] << ")" << std::endl; // error!
+    REQUIRE(mv4.size() == 10);
+    REQUIRE(mv4[0] == 777);
+    REQUIRE(mv4[9] == 777);
 }
